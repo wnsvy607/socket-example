3a50b64295c4b041e7ed56df4f1b4b161976c0ec
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2025-05-06 21:25:01 +0900

    TCP Socket Echo example

---
+++ TCPServer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+)
+
+func main() {
+	serverPort := "20912"
+
+	listener, _ := net.Listen("tcp", ":"+serverPort)
+	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+
+	buffer := make([]byte, 1024)
+
+	for {
+
+		conn, _ := listener.Accept()
+		fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
+
+		count, _ := conn.Read(buffer)
+
+		conn.Write(bytes.ToUpper(buffer[:count]))
+
+		conn.Close()
+	}
+}
